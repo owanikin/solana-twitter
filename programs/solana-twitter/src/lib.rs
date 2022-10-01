4647d4e6abcf0dea39f3d2e67487cabf273e0ad1
Owanikin <oderindeife@gmail.com>
2022-10-01 20:29:00 +0100

    guarding against invalid data

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -11,16 +11,32 @@ pub mod solana_twitter {
     //     Ok(())
     // }
 
+    #[error_code]
+    pub enum ErrorCode {
+        #[msg("The provided topic should be 50 characters long maximum.")]
+        TopicTooLong,
+        #[msg("The provided content should be 280 characters long maximum.")]
+        ContentTooLong,
+    }
+
     pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
         let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
         let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
 
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into())
+        }
+
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into())
+        }
+
         tweet.author = *author.key;
         tweet.timestamp = clock.unix_timestamp;
         tweet.topic = topic;
         tweet.content = content;
-        
+
         Ok(())
     }
 }
